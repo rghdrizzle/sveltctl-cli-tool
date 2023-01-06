0284716ea21527bbe46ae221e3899d67c51ff44b
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2023-01-06 15:51:45 +0530

    added a command to startup a dev server

--- sveltctl.py
+++ sveltctl.py
@@ -31,12 +31,19 @@ def tailbuild(config,output_file,style):
 @cli.command()
 @click.argument('input_file')
 @click.argument('output_file')
-def render(input_file, output_file):
+def render(input_file, output_file): #for rendering a sveltefile and then producing the output to a output file
     with open(input_file, 'r') as f:
         component = f.read()
     output = sveltekit.render(component)
     with open(output_file, 'w') as f:
         f.write(output)
 
+@cli.command()
+@click.argument('appdir')
+def run_dev(appdir): #for starting a development server with hot reloading enabled
+    sveltekit.dev({
+        'rootDir': appdir,
+        'hotReloading': True,
+    })
 if __name__ =="__main__":
     cli()
